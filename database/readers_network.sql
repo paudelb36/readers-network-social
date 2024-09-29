6b5d1368deb5f3d9706f597c606e0bac9be5dbd7
paudelb36 <paudelb11@gmail.com>
2024-09-29 10:58:47 +0545

    db

--- database/readers_network.sql
+++ database/readers_network.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 29, 2024 at 07:09 AM
+-- Generation Time: Sep 29, 2024 at 07:13 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
